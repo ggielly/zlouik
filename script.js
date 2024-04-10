55f8b8dcfd8baa16095bc2a0cfce1ce43ead090a
guillaume@gielly.fr <guillaume@gielly.fr>
2024-04-10 11:03:16 +0200

    Suppression des vieux fichiers.

--- script.js
+++
@@ -1,9 +0,0 @@
-function updatePrimeMontant() {
-    const primeMontant01 = document.getElementById("prime_montant_01").value;
-    document.getElementById("tns_ir_cell3_1").innerHTML = primeMontant01 + " €";
-    
-    // Affichage d'informations supplémentaires
-    // const montantTotal = primeMontant01 * 1.2; // TVA 20%
-    // document.getElementById("montant_total").innerHTML = montantTotal + " €";
-}
-
